95a752ead5ad14d3dec6ba2e6521174506d999e5
protondk <edgarjosue2911@gmail.com>
2023-03-30 19:28:49 -0600

    Creating CRUD: Creating tasks. Tasks are being added to task list. Unique IDs are being created for each task. Each task checkbox also has its corresponding label for task id.

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
 // @ts-nocheck
+// @ts-nocheck
 const input = document.getElementById("taskInput");
-const taskList = [];
+const taskList = document.getElementById("taskList");
+let taskId = 0;
 
 const addTask = (event) => {
   if (event.keyCode === 13) {
-    if (input) {
+    /* This is checking if the input is empty. If it is not empty, it will create a new task object with
+the task and completed properties. It will then append the task to the task list. */
+    if (input.value.trim() !== "") {
       const newTask = {
         task: input.value,
         completed: false,
       };
-      taskList.push(newTask);
+      taskList.appendChild(createTaskElement(newTask));
       input.value = "";
-      console.log(taskList);
     }
   }
+};
+
+const createTaskElement = (task) => {
+  const taskElement = document.createElement("div");
+  taskElement.classList.add("task-item");
+  // Setting an ID to each task
+  taskElement.dataset.id = taskId++;
+
+  const taskItemContent = `
+<div class="checkbox-wrapper">
+  <label class="container">
+    <input type="checkbox" id="checkbox-${taskId}" class="checkbox visually-hidden">
+    <span class="checkmark"></span>
+  </label>
+</div>
+<div class="task-wrapper">
+  <p class="task-text">${task.task}</p>
+</div>
+<div class="edit-delete-wrapper">
+  <i class="fa-solid fa-pen-to-square edit-icon"></i>
+  <i class="fa-solid fa-trash-can delete-icon"></i>
+</div>
+`;
+
+  taskElement.innerHTML = taskItemContent;
+
+  console.log(taskElement.dataset.id);
+  return taskElement;
 };
 
 input.addEventListener("keyup", addTask);
